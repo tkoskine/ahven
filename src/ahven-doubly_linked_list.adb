6fd0a1b42b35f87183a96c5cdb6f108f5185577d
tkoskine <none@none>
2008-01-02 15:33:18 +0000

    Update copyright years.

--- src/ahven-doubly_linked_list.adb
+++ src/ahven-doubly_linked_list.adb
@@ -1,5 +1,5 @@
 --
--- Copyright (c) 2006, 2007 Tero Koskinen <tero.koskinen@iki.fi>
+-- Copyright (c) 2006, 2007, 2008 Tero Koskinen <tero.koskinen@iki.fi>
 --
 -- Permission to use, copy, modify, and distribute this software for any
 -- purpose with or without fee is hereby granted, provided that the above
