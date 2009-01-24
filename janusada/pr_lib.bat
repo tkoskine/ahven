f10a4cec151d5c198a27d5f068590ecd09b7ffd2
Tero Koskinen <tero.koskinen@iki.fi>
2009-01-24 22:19:36 +0200

    Use a separate januspath variable for pointing out the installation directory of Janus/Ada.

--- janusada/pr_lib.bat
+++ janusada/pr_lib.bat
@@ -1,4 +1,5 @@
+set januspath=C:\Janus312\rts\console
 del ..\objects\*.*
 jmanager Add_Project (..\Objects\,AhvenLib)
-jmanager Add_Link (..\Objects\,AhvenLib,C:\Janus312\rts\console, JNT_RTS_CONSOLE)
+jmanager Add_Link (..\Objects\,AhvenLib,%januspath%, JNT_RTS_CONSOLE)
 
