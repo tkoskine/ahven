8a1b2fc894e8256fa2b7800b568cc0904d65c197
Tero Koskinen <tero.koskinen@iki.fi>
2009-01-24 22:18:12 +0200

    Project links are transitive, link to runtime is not needed here.

--- janusada/pr_test.bat
+++ janusada/pr_test.bat
@@ -1,4 +1,3 @@
 del ..\test_objects\*.*
 jmanager Add_Project (..\Test_Objects\,AhvenTst)
-jmanager Add_Link (..\Test_Objects\,AhvenTst,C:\Janus312\rts\console, JNT_RTS_CONSOLE)
 jmanager Add_Link (..\Test_Objects\,AhvenTst,..\Objects, AhvenLib)
