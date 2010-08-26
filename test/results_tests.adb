16fe42704aa6438b5a2f677f1059447267e771a8
Tero Koskinen <tero.koskinen@iki.fi>
2010-08-26 21:04:14 +0300

    Bump copyright year to 2010.

--- test/results_tests.adb
+++ test/results_tests.adb
@@ -1,5 +1,5 @@
 --
--- Copyright (c) 2008-2009 Tero Koskinen <tero.koskinen@iki.fi>
+-- Copyright (c) 2008-2010 Tero Koskinen <tero.koskinen@iki.fi>
 --
 -- Permission to use, copy, modify, and distribute this software for any
 -- purpose with or without fee is hereby granted, provided that the above
