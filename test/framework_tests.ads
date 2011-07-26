200c97e0c5e7b74b370c2b4d867cdcd2c3c856ce
Tero Koskinen <tero.koskinen@iki.fi>
2011-07-26 21:27:11 +0300

    Add two tests for the timeout feature.
    The first test checks does the timeout work with "delay" statement
    and the second test checks are the infinite loops breakable.
    At the moment, GNAT on Linux fails the infinite loop test.

--- test/framework_tests.ads
+++ test/framework_tests.ads
@@ -35,6 +35,10 @@ private
 
    procedure Test_Test_Case_Run;
 
+   procedure Test_Test_Case_Run_1s_Timeout;
+
+   procedure Test_Test_Case_Run_Break_Infinite_Loop;
+
    procedure Test_Test_Case_Test_Count;
 
    procedure Test_Test_Case_Truncate_Name;
