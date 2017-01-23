23567707fc3f343719a5a81d0b289d5ecd1929a8
Tero Koskinen <tero.koskinen@iki.fi>
2017-01-23 22:10:01 +0200

    Adjust rules for Adacontrol V1.18r9.

---
+++ run_adacontrol.bat
@@ -0,0 +1,6 @@
+del objects\*.adt
+del objects\*.ali
+mkdir objects
+cd objects
+adactl -p ..\gnat\ahven.gpr -f ..\rules\ahven.aru libmain Ahven Ahven.Framework Ahven.Listeners Ahven.Listeners.Basic Ahven.Parameters Ahven.Results Ahven.Runner Ahven.Slist Ahven.Tap_Runner Ahven.Text_Runner Ahven.Xml_Runner Ahven.Astrings Ahven.Long_AStrings Ahven.Name_List Ahven_Compat Ahven.Temporary_Output
+cd ..
