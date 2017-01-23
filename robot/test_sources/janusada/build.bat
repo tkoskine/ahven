ff1e8530329e68734c759bf6a2437877e4c426b3
Tero Koskinen <tero.koskinen@iki.fi>
2017-01-23 21:51:13 +0200

    Add fail1 to Janus/Ada build scripts.

--- robot/test_sources/janusada/build.bat
+++ robot/test_sources/janusada/build.bat
@@ -5,3 +5,11 @@ call ctst.bat
 call lkc simple1r
 move simple1r.exe ..
 cd ..
+
+cd fail1
+corder fail1r /pfail1/l'ads'/n'adb'/t/w/k255/js'jbind'/jb'/t/l/YLLIBCMT'/b'..\fail1_obj\ctst.bat'/r..\fail1_obj
+cd ..\fail1_obj
+call ctst.bat
+call lkc fail1r
+move fail1r.exe ..
+cd ..
--- robot/test_sources/janusada/prepare.bat
+++ robot/test_sources/janusada/prepare.bat
@@ -1,8 +1,18 @@
-cd simple1
 set januspath=C:\JanusAda\rts
+
+cd simple1
 del /q ..\simple1_obj\*.*
+del /q ..\fail1_obj\*.*
 mkdir ..\simple1_obj
 jmanager Add_Project (..\simple1_obj\,Simple1)
 jmanager Add_Link (..\simple1_obj\,Simple1,%januspath%, JNT_RTS)
 jmanager Add_Link (..\simple1_obj\,Simple1,..\..\..\lib_obj\, AhvenLib)
+cd ..
+
+cd fail1
+del /q ..\fail1_obj\*.*
+mkdir ..\fail1_obj
+jmanager Add_Project (..\fail1_obj\,fail1)
+jmanager Add_Link (..\fail1_obj\,fail1,%januspath%, JNT_RTS)
+jmanager Add_Link (..\fail1_obj\,fail1,..\..\..\lib_obj\, AhvenLib)
 cd ..
