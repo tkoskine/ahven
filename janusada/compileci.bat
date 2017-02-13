4b57396470a8cc199be6622e6922ca7626bae433
Tero Koskinen <tero.koskinen@iki.fi>
2017-02-13 22:51:09 +0200

    Move CI compile scripts to object directories.

--- janusada/compileci.bat
+++ janusada/compileci.bat
@@ -1,15 +1,12 @@
-cd src\windows
-call ctst.bat
+call com_obj\ctst.bat
 if ErrorLevel 2 goto Abort
-cd ..
-call ctst.bat
+call lib_obj\ctst.bat
 if ErrorLevel 2 goto Abort
-cd ..\test
-call ctst.bat
+call test_obj\ctst.bat
 if ErrorLevel 2 goto Abort
-call ctsttap.bat
+call test_obj\ctsttap.bat
 if ErrorLevel 2 goto Abort
-cd ..\test_obj
+cd test_obj
 link  -subsystem:console -entry:mainCRTStartup -out:tap_test.exe tap_test.obj libcmt.lib kernel32.lib user32.lib -map:tap_test.map
 link  -subsystem:console -entry:mainCRTStartup -out:tester.exe tester.obj libcmt.lib kernel32.lib user32.lib -map:tap_test.map
 cd ..
