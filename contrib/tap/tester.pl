8cf070183250dc095fac2ea24d74bb3669312667
Tero Koskinen <tero.koskinen@iki.fi>
2021-03-25 01:32:17 +0200

    Improve control/tap/tester.pl a little.
    Add ability to give the test runner as an argument.
    Also do some sanity checks and return status based
    on the test execution status.

--- contrib/tap/tester.pl
+++ contrib/tap/tester.pl
@@ -3,13 +3,29 @@
 use TAP::Parser;
 use TAP::Parser::Aggregator;
 
-my $parser = TAP::Parser->new( { exec => [ './tap_tester' ]  } );
+my $tester_exe = shift or die "usage: $0 \<tap_test.exe\>\n";
+
+if (! -x $tester_exe ) {
+	print "No test executable $tester_exe\n";
+	exit(255);
+}
+
+my $parser = TAP::Parser->new( { exec => [ $tester_exe ] } );
+
 $parser->run;
+
+if ($parser->tests_planned == 0) {
+	print "No planned tests\n";
+	exit(255);
+}
+
 # while ( my $result = $parser->next ) {
-#     print $result->as_string . "\n";
+#    print $result->as_string . "\n";
 # }
 
 my $aggregate = TAP::Parser::Aggregator->new;
 $aggregate->add( 'testcases', $parser );
-printf "\tPassed: %s\n\tFailed: %s\n", scalar $aggregate->passed, scalar $aggregate->failed;
+printf "\tPassed: %s\n\tFailed: %s\n\tSkipped: %s\n", scalar $aggregate->passed, scalar $aggregate->failed, scalar $aggregate->skipped;
+printf "\tTests took %d seconds\n", ($parser->end_time - $parser->start_time);
 
+exit($parser->exit);
