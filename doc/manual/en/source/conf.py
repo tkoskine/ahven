07170ea153046ab51eea33e99aa3b98f0231aed9
Tero Koskinen <tero.koskinen@iki.fi>
2021-08-16 22:52:12 +0300

    Increase version to 2.9 everywhere.
    As done with Alire project file, increate the version to 2.9
    in the documentation and Linux (Fedora) GNAT project files.

--- doc/manual/en/source/conf.py
+++ doc/manual/en/source/conf.py
@@ -42,9 +42,9 @@ copyright = '2018, Tero Koskinen'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.8'
+version = '2.9'
 # The full version, including alpha/beta/rc tags.
-release = '2.8'
+release = '2.9'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
